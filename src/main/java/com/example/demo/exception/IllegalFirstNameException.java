38e185770435c3978f5e6b2467a4a8da95e50f3f
Shivani-96 <shivanisah96@gmail.com>
2020-11-25 12:07:31 +0530

    removed unwanted code

--- src/main/java/com/example/demo/exception/IllegalFirstNameException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-public class IllegalFirstNameException extends Exception{
-
-	public IllegalFirstNameException() {
-		super("first name is invalid");
-	}
-
-}
--- src/main/java/com/example/demo/exception/IllegalLastNameException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class IllegalLastNameException extends Exception {
-	
-	public IllegalLastNameException() {
-		super("last name is invalid");
-	}
-}
